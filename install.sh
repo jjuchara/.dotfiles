3c1095d755d6fe4f7f6e58943305e7ab312c3c78
jjuchara <jjuchara@gmail.com>
2022-04-25 14:08:29 +0300

    initial

---
+++ install.sh
@@ -0,0 +1,48 @@
+# install nix
+curl -L https://nixos.org/nix/install | sh
+
+# source nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.git \
+	nixpkgs.neovim \
+	nixpkgs.tmux \
+	nixpkgs.stow \
+	nixpkgs.yarn \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.gnumake \
+	nixpkgs.gcc \
+	nixpkgs.direnv\
+  nixpkgs.alacritty\
+  nixpkgs.python310
+
+# stow dotfiles
+stow git
+stow vim
+stow nvim
+stow ideavim
+stow tmux
+stow zsh
+stow alacritty
+stow zshrc
+stow yabai
+
+# add zsh as a login shell
+command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell
+sudo chsh -s $(which zsh) $USER
+
+# # bundle zsh plugins 
+# antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+#
+# # install neovim plugins
+# nvim --headless +PlugInstall +qall
+
+# Use kitty terminal on MacOS
+# [ `uname -s` = 'Darwin' ] && stow kitty
