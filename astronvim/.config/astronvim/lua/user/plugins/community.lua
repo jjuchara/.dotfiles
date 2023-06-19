d6d4daac5e1f9b12ca663d6cffebe071c54349cf
Jjuchara <jjuchara@gmail.com>
2023-06-19 14:23:26 +0300

    delete comunity plugins for colourscheme

--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -1,8 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity/code-runner/sniprun" },
-  { import = "astrocommunity/colorscheme/tokyonight" },
-  { import = "astrocommunity/colorscheme/gruvbox" },
   { import = "astrocommunity/colorscheme/catppuccin" },
   { import = "astrocommunity/motion/nvim-surround/" },
 }
